dc8e6082420ebc16692c0ec1882485af541df9c7
fengyingjie <847369954@qq.com>
2017-08-25 09:56:01 +0800

    免费订单

--- pages/historyOrder/historyorderInfo/index.js
+++ pages/historyOrder/historyorderInfo/index.js
@@ -36,8 +36,33 @@ Page({
         success: function (res) {
           wx.setStorageSync('timestamp', res.data.timestamp);//缓存时间戳
           console.log("请求结果", res)
+          var orderType;
+          if (res.data.Edata[0].data.orderType == 1) {
+            orderType = "共享收益";
+          } else if (res.data.Edata[0].data.orderType == 2) {
+            orderType = "充电费用";
+          } else if (res.data.Edata[0].data.orderType == 3) {
+            orderType = "预约超时收益";
+          } else if (res.data.Edata[0].data.orderType == 4) {
+            orderType = "免费";
+          } else if (res.data.Edata[0].data.orderType == 5) {
+            orderType = "预约超时扣费";
+          }
+          var days = Math.floor(res.data.Edata[0].data.timeTotal / 1440) == 0 ? "" : Math.floor(res.data.Edata[0].data.timeTotal / 1440) + "天";
+          var hourtime = res.data.Edata[0].data.timeTotal - Math.floor(res.data.Edata[0].data.timeTotal / 1440) * 1440;
+          var hours = Math.floor(hourtime / 60) == 0 ? "" : Math.floor(hourtime / 60) + "小时";
+          var mintime = (hourtime - Math.floor(hourtime / 60) * 60) + "分";
+          var timeTotal = days + hours + mintime;
+          console.log(res.data.Edata[0].data)
           that.setData({
-            historyId: res.data.Edata[0].data.orderId//明细列表
+            historyId: res.data.Edata[0].data.orderId,
+            historydeviceId: res.data.Edata[0].data.deviceName + "-" + res.data.Edata[0].data.deviceSN,
+            orderType: orderType,
+            startTime: res.data.Edata[0].data.startTime,
+            endTime: res.data.Edata[0].data.endTime,
+            timeTotal: timeTotal,
+            electricity: (res.data.Edata[0].data.electricity * 0.01).toFixed(2) + "kWh",
+            amountHiden: true
           })
         },
         fail: function (res) {
@@ -88,6 +113,7 @@ Page({
             timeTotal: timeTotal,
             electricity: (res.data.Edata[0].data.electricity * 0.01).toFixed(2)+"kWh",
             amount: (res.data.Edata[0].data.expenses[0].amount * 0.01).toFixed(2) + "元",
+            amountHiden: false
           })
         },
         fail: function (res) {
